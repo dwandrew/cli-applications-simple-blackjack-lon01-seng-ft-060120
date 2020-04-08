9b1afbfc394463d2b9d60edc18ecf58fb982015c
Daniel Andrew <dwandrew@hotmail.com>
2020-04-08 09:52:34 +0000

    Automatically backed up by Learn

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,14 +1,3 @@
 require_relative "blackjack.rb"
 
-def runner
-  welcome
-  total = initial_round
-  if total < 21
-  total= hit?(total)
-  display_card_total(total)
-  end
-  if total == 21
-  end_game(total)
-  end
-  end_game(total)
-end
+runner
